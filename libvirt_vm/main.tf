7d1ec63ec3d94af3faf98b3b4551b5addd550b07
Travis Lyons <travis.lyons@gmail.com>
2020-07-13 07:52:39 -0400

    Add libvirt_vm module

---
+++ libvirt_vm/main.tf
@@ -0,0 +1,85 @@
+resource "libvirt_volume" "os" {
+  count          = var.instances
+  name           = "${var.name}${count.index + 1}"
+  pool           = var.os_image.pool
+  format         = var.os_image.format
+  size           = var.os_image.size
+  base_volume_id = var.os_image.image_volume_id
+}
+
+resource "libvirt_volume" "data" {
+  for_each = {
+    for item in setproduct((range(1, var.instances)), var.additional_volumes) :
+    "${var.name}${item[0]}-${item[1].name}" => item[1]
+  }
+  name   = each.key
+  pool   = each.value.pool
+  size   = each.value.size
+  format = each.value.format
+}
+
+resource "libvirt_cloudinit_disk" "cloud_init" {
+  count = var.instances
+  name  = "${var.name}${count.index + 1}-cloud-init.iso"
+  pool  = var.cloud_init_pool
+  user_data = templatefile(
+    "./templates/cloud_config.cfg",
+    {
+      hostname     = "${var.name}${count.index + 1}"
+      initial_user = var.initial_user
+    }
+  )
+  network_config = templatefile(
+    "./templates/network_config.cfg",
+    {
+      networks = var.networks
+    }
+  )
+}
+
+resource "libvirt_domain" "vm" {
+  count      = var.instances
+  name       = "${var.name}${count.index + 1}"
+  memory     = var.memory
+  vcpu       = var.vcpu
+  qemu_agent = true
+
+  cloudinit = libvirt_cloudinit_disk.cloud_init[count.index].id
+
+  dynamic "network_interface" {
+    for_each = var.networks
+    content {
+      network_id     = network_interface.value.network_id
+      wait_for_lease = true
+    }
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  disk {
+    volume_id = libvirt_volume.os[count.index].id
+  }
+
+  dynamic "disk" {
+    for_each = var.additional_volumes
+    content {
+      volume_id = libvirt_volume.data["${var.name}${count.index + 1}-${disk.value.name}"].id
+    }
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+}
---
+++ libvirt_vm/variables.tf
@@ -0,0 +1,75 @@
+variable "name" {
+  type        = string
+  description = "Virtual machine name"
+}
+
+variable "instances" {
+  type        = number
+  description = "Number of instances to create"
+  default     = 1
+}
+
+variable "memory" {
+  type        = number
+  default     = 512
+  description = "The amount of memory in MB"
+}
+
+variable "vcpu" {
+  type        = number
+  default     = 1
+  description = "The number of virtual CPUs."
+}
+
+variable "autostart" {
+  type        = bool
+  description = "Enable/Disable autostart of the VM on host boot"
+  default     = true
+}
+
+variable "networks" {
+  type = list(object({
+    network_id  = string,
+    interface   = string,
+    ip_address  = string,
+    gateway     = string,
+    nameservers = list(string),
+    domain      = string
+  }))
+  description = "List of networks to configure"
+  default     = []
+}
+
+variable "os_image" {
+  type = object({
+    image_volume_id = string
+    size            = number
+    pool            = string
+    format          = string
+  })
+}
+
+variable "additional_volumes" {
+  type = set(object({
+    name   = string
+    size   = number
+    pool   = string
+    format = string
+  }))
+  default = []
+}
+
+variable "cloud_init_pool" {
+  type = string
+}
+
+variable "initial_user" {
+  type = object({
+    username = string,
+    gecos    = string,
+    groups   = set(string),
+    shell    = string,
+    sudo     = string,
+    ssh_keys = set(string)
+  })
+}
